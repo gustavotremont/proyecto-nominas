7b90ca9fe59bac3dfb06a405bb699270ee9834da
rickyzu <bescir@gmail.com>
2021-11-04 13:57:25 +0100

    create index.js

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCnU6x3fdfYZg8idyaUsrK-cLppVqr1pOo",
-  authDomain: "proyectonomina-80b46.firebaseapp.com",
-  projectId: "proyectonomina-80b46",
-  storageBucket: "proyectonomina-80b46.appspot.com",
-  messagingSenderId: "786867662699",
-  appId: "1:786867662699:web:d84687d70afdfc5825a62e"
+    apiKey: 'AIzaSyCnU6x3fdfYZg8idyaUsrK-cLppVqr1pOo',
+    authDomain: 'proyectonomina-80b46.firebaseapp.com',
+    projectId: 'proyectonomina-80b46',
+    storageBucket: 'proyectonomina-80b46.appspot.com',
+    messagingSenderId: '786867662699',
+    appId: '1:786867662699:web:d84687d70afdfc5825a62e',
 };
 
 // Initialize Firebase
